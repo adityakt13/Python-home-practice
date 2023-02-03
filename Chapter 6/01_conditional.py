651d3be05e0a4739bad38799a6096ec0e8fd3bc6
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-03 18:07:52 +0530

    conditional statement

---
+++ Chapter 6/01_conditional.py
@@ -0,0 +1,9 @@
+a = 15
+if a >3:
+    print("The value of a is greater than 3")
+elif a >7:
+    print("The value of a is greater than 7")
+elif a < 21:
+    print("The value of a is not greater than 21")
+else:
+    print("The value of a is not greater than 3 or 7")
