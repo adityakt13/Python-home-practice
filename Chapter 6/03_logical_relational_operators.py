ce6e3658267c432aa66db4e003a4056e0755e08c
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-03 18:08:34 +0530

    logical and relational operators

---
+++ Chapter 6/03_logical_relational_operators.py
@@ -0,0 +1,11 @@
+age = int(input("enter your age: "))
+if age <18 :
+    print("You are still a minor.")
+elif age >=18 and age <= 25:
+    print("Study hard and find a job.")
+elif age > 25 and age <=45:
+    print("This is a perfect time to work.")
+elif age >45 and age <57 :
+    print("You should plan for your retirement.")
+else:
+    print("Happy retirement.")
