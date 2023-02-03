dcd1afaf5cc682c38b554e9e71d8453bcbe6cf6d
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-03 17:35:27 +0530

    prime number

--- Chapter 6/08_pr_03.py
+++ Chapter 6/08_pr_03.py
@@ -1,4 +1,4 @@
-comment = input("Enter a text and try to avoid spam: ")
+comment = input("Enter a text and try to avoid spam: ") 
 if ("make a lot of money", "buy now", "subscribe this", "click this" in comment):
     print("This comment contains a spam")
 else:
