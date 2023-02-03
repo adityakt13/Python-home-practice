9d11459c5172ddfe566a000808d0f003290b98c4
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-03 17:36:10 +0530

    checking name

--- Chapter 6/10_pr_05_name_check.py
+++ Chapter 6/10_pr_05_name_check.py
@@ -2,5 +2,5 @@ names = ["Aditya", "Xaid", "Udbhav","Avneesh","Amrit","Asit","Bharvi"]
 name = input("Input a name: ")
 if name.capitalize() in names:
     print("This name is in the names list")
-else:
+else: 
     print("This name is not in the names list")
