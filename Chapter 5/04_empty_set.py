03eb85141480834e243a9d5253050404bf04c23c
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-04 16:04:23 +0530

    empty set

---
+++ Chapter 5/04_empty_set.py
@@ -0,0 +1,11 @@
+a = {}
+print(type(a))
+a.update({1:"hello"})
+print(a)
+# if we straight away just use curly braces , it will create an empty dictionary
+
+# creating an empty set
+s = set()
+print(type(s))
+s.add(6)
+print(s)
