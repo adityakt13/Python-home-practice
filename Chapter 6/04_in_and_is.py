ebd3ca2c54915e2ee9568458907c0b5da1677e35
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-04 16:05:51 +0530

    in and is

---
+++ Chapter 6/04_in_and_is.py
@@ -0,0 +1,16 @@
+a = None
+if a is None:
+    print("yes")
+else:
+    print("no")
+
+b = [56,98,63]
+if 45 in b :
+    print("True")
+else:
+    print("False")
+
+if type(b) is list:
+    print("correct")
+else:
+    print("wrong")
