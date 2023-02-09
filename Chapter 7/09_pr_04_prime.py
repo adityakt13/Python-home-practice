0fbe1c557a1f8571958e431a4f80bb209fe08122
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-09 20:32:18 +0530

    prime number

--- Chapter 7/09_pr_04_prime.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number")) 
-if num> 1:
-    for i in range(2,num):
-        if num % i != 0:
-            print("This is a prime number")
-            break
-    else:
-        print("Not a prime number")
