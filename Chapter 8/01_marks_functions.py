1b97b56cd6549a607943851d62667b4bfb5d377e
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-11 22:35:25 +0530

    mark function

---
+++ Chapter 8/01_marks_functions.py
@@ -0,0 +1,14 @@
+def percent(marks):
+    return sum(marks)/len(marks) 
+
+marks1 = [45, 78, 86, 77]
+percentage1 = percent(marks1)
+print(percentage1)
+
+marks2 = [12,90,99,77,45]
+percentage2 = percent(marks2)
+print(percentage2)
+
+marks3 = [56,90,98,93,90]
+percentage3 = percent(marks3)
+print(percentage3)
