929f2d16a88dc2cb8f2c526241b6dd784136d9f2
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-13 20:23:24 +0530

    marks function

--- Chapter 8/01_marks_functions.py
+++ Chapter 8/01_marks_functions.py
@@ -6,7 +6,7 @@ percentage1 = percent(marks1)
 print(percentage1)
 
 marks2 = [12,90,99,77,45]
-percentage2 = percent(marks2)
+percentage2 = percent(marks2) 
 print(percentage2)
 
 marks3 = [56,90,98,93,90]
