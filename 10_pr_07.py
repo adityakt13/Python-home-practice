fe50d4ac2296c0d423ae1cff857157c23b9fbfaf
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-21 00:31:37 +0530

    split function

--- 10_pr_07.py
+++ 10_pr_07.py
@@ -1,7 +1,8 @@
 def remove_and_split (string,word):
     newstr = string.replace(word, "")
+    return newstr.strip()
 
 this = "   Aditya is a good boy  "
-remove_and_split(this, Aditya)
-print(this)
-print(this.strip())
+remove_and_split(this, "Aditya")
+n = remove_and_split(this, "Aditya")
+print(n)
