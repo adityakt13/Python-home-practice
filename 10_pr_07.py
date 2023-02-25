7383955d62cc3d45bd2f7bc9db2c73c7d2519766
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-02-25 23:21:36 +0530

    split

--- 10_pr_07.py
+++
@@ -1,8 +0,0 @@
-def remove_and_split (string,word):
-    newstr = string.replace(word, "")
-    return newstr.strip()
-
-this = "   Aditya is a good boy  "
-remove_and_split(this, "Aditya")
-n = remove_and_split(this, "Aditya")
-print(n)
