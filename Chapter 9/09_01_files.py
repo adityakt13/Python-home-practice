0d52234a92dbc56ea29f4551524ad5fd9a05202b
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-03-22 19:22:52 +0530

    reading file

--- Chapter 9/09_01_files.py
+++ Chapter 9/09_01_files.py
@@ -1,5 +1,7 @@
 # Use open function to read the content of a file!
-f = open('name.txt')
-data = f.read()
+# f = open('sample.txt', 'r')
+f = open('sample.txt') # by default the mode is 'r'
+# data = f.read()
+data = f.read() # will read only first 5 characters of the file
 print(data)
 f.close()
