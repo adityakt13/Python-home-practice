4e668d8b832b5531782d73363e46b7340e8affb7
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-03-23 23:24:53 +0530

    reading different lines

--- Chapter 9/09_02_readline.py
+++ Chapter 9/09_02_readline.py
@@ -1,5 +1,13 @@
 f = open('sample.txt')
 data = f.readline() # it will read the first line
 print(data)
+
 data = f.readline() # it will read the second line
 print(data)
+
+data = f.readline() # it will read the third line
+print(data)
+
+data = f.readline() # it will read the fourth line
+print(data)
+f.close()
