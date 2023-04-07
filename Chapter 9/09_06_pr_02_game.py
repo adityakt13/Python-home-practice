3a8d3cd68ec8741a04929bdf44fe7c32ebd54724
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-04-07 12:53:57 +0530

    game.py

--- Chapter 9/09_06_pr_02_game.py
+++ Chapter 9/09_06_pr_02_game.py
@@ -4,7 +4,7 @@ def game ():
 score = game()
 with open("highscore.txt") as f:
     highscoreStr = f.read()
- 
+
 if int(highscoreStr) < score:
     with open("highscore.txt", "w") as f:
         f.write(str(score))
