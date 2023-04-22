6a54dbb748eb3ce5bc49fb1807e24111016ae795
adityakt13 <118040130+adityakt13@users.noreply.github.com>
2023-04-22 23:55:46 +0530

    Delete 09_08_pr_05_replace_multi.py

--- Chapter 9/09_08_pr_05_replace_multi.py
+++
@@ -1,9 +0,0 @@
-words = ['donkey','horse','zebra','mule']
-with open ("sample.txt") as f:
-    content = f.read() 
-
-for word in words:
-    content = content.replace(word, "o(^.^)o")
-
-with open("sample.txt", "w") as f:
-    f.write(content)
